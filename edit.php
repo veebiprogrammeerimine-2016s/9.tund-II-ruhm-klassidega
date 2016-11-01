9fc959f5c452f791e8b61851419238b815121b6f
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-11-01 13:59:29 +0200

    added inital files

---
+++ edit.php
@@ -0,0 +1,63 @@
+<?php
+	//edit.php
+	require("functions.php");
+	require("editFunctions.php");
+	
+	//var_dump($_POST);
+	
+	//kas kasutaja uuendab andmeid
+	if(isset($_POST["update"])){
+		
+		updateCar(cleanInput($_POST["id"]), cleanInput($_POST["plate"]), cleanInput($_POST["color"]));
+		
+		header("Location: edit.php?id=".$_POST["id"]."&success=true");
+        exit();	
+		
+	}
+	
+	//kustutan
+	if(isset($_GET["delete"])){
+		
+		deleteCar($_GET["id"]);
+		
+		header("Location: data.php");
+		exit();
+	}
+	
+	
+	
+	// kui ei ole id'd aadressireal siis suunan
+	if(!isset($_GET["id"])){
+		header("Location: data.php");
+		exit();
+	}
+	
+	//saadan kaasa id
+	$c = getSingleCarData($_GET["id"]);
+	//var_dump($c);
+	
+	if(isset($_GET["success"])){
+		echo "salvestamine õnnestus";
+	}
+
+	
+?>
+<br><br>
+<a href="data.php"> tagasi </a>
+
+<h2>Muuda kirjet</h2>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+	<input type="hidden" name="id" value="<?=$_GET["id"];?>" > 
+  	<label for="number_plate" >auto nr</label><br>
+	<input id="number_plate" name="plate" type="text" value="<?php echo $c->plate;?>" ><br><br>
+  	<label for="color" >värv</label><br>
+	<input id="color" name="color" type="color" value="<?=$c->color;?>"><br><br>
+  	
+	<input type="submit" name="update" value="Salvesta">
+  </form>
+  
+  
+ <br>
+ <br>
+ <br>
+ <a href="?id=<?=$_GET["id"];?>&delete=true">kustuta</a>
---
+++ editFunctions.php
@@ -0,0 +1,88 @@
+<?php
+
+	require_once("../../config.php");
+	
+	function getSingleCarData($edit_id){
+    
+        $database = "if16_romil";
+
+		//echo "id on ".$edit_id;
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+		
+		$stmt = $mysqli->prepare("SELECT plate, color FROM cars_and_colors WHERE id=? AND deleted IS NULL");
+
+		$stmt->bind_param("i", $edit_id);
+		$stmt->bind_result($plate, $color);
+		$stmt->execute();
+		
+		//tekitan objekti
+		$car = new Stdclass();
+		
+		//saime ühe rea andmeid
+		if($stmt->fetch()){
+			// saan siin alles kasutada bind_result muutujaid
+			$car->plate = $plate;
+			$car->color = $color;
+			
+			
+		}else{
+			// ei saanud rida andmeid kätte
+			// sellist id'd ei ole olemas
+			// see rida võib olla kustutatud
+			header("Location: data.php");
+			exit();
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $car;
+		
+	}
+
+
+	function updateCar($id, $plate, $color){
+    	
+        $database = "if16_romil";
+
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+		
+		$stmt = $mysqli->prepare("UPDATE cars_and_colors SET plate=?, color=? WHERE id=? AND deleted IS NULL");
+		$stmt->bind_param("ssi",$plate, $color, $id);
+		
+		// kas õnnestus salvestada
+		if($stmt->execute()){
+			// õnnestus
+			echo "salvestus õnnestus!";
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	
+	function deleteCar($id){
+    	
+        $database = "if16_romil";
+
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+		
+		$stmt = $mysqli->prepare("UPDATE cars_and_colors SET deleted=NOW() WHERE id=? AND deleted IS NULL");
+		$stmt->bind_param("i",$id);
+		
+		// kas õnnestus salvestada
+		if($stmt->execute()){
+			// õnnestus
+			echo "kustutamine õnnestus!";
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	
+	
+?>
